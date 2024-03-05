a0f7040b69331873392e1996aea5f63c07d2077c
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-03-05 19:51:39 +0800

    added rectstrap to header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddHabit from './components/AddHabit'
-import NavBar from './components/NavBar';
+import Header from './components/NavBar';
 import ViewWeekly from './components/ViewWeekly';
 
 function App() {
   return (
     <>
-    <NavBar />
+    <Header />
     <div className="container mt-5">
       <Routes>
         <Route path="/" element={<AddHabit />} />
-        <Route path="/view-weekly" element={<ViewWeekly  />} />
+        <Route path="/view-weekly" element={<ViewWeekly/>} />
       </Routes>
     </div>
   </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,77 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { 
+    Navbar, 
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav, 
+    NavItem,
+} from 'reactstrap';
 
-const Navbar = () => {
+const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     };
 
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <div className="container">
-                <Link className="navbar-brand" to="/">
-                    Habit Tracker
-                </Link>
-                <button onClick={toggleNavbar} className= 'navbar-toggler' type="button"  >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                    <div isOpen={isOpen} className="collapse navbar-collapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">
-                                    Add New Habit
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/view-weekly">
-                                    View Weekly
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+    // return(
+    //     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    //         <div className="container">
+    //             <Link className="navbar-brand" to="/">
+    //                 Habit Tracker
+    //             </Link>
+    //             <button onClick={toggleNavbar} className= 'navbar-toggler' type="button"  >
+    //                 <span className="navbar-toggler-icon"></span>
+    //             </button>
+    //                 <div isOpen={isOpen} className="collapse navbar-collapse">
+    //                     <ul className="navbar-nav mr-auto">
+    //                         <li className="nav-item">
+    //                             <Link className="nav-link" to="/">
+    //                                 Add New Habit
+    //                             </Link>
+    //                         </li>
+    //                         <li className="nav-item">
+    //                             <Link className="nav-link" to="/view-weekly">
+    //                                 View Weekly
+    //                             </Link>
+    //                         </li>
+    //                     </ul>
+    //                 </div>
                     
-            </div>
-        </nav>
+    //         </div>
+    //     </nav>
+    // );
+
+    return(
+        <Navbar dark expand='md' className="" sticky='top' color='primary'>
+            
+                <NavbarBrand className="ps-5" href='/'>
+                    Habit Tracker
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar}   />
+                    {/* <span className="navbar-toggler-icon"></span> */}
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="navbar-nav">
+                            <NavItem className="nav-item">
+                                <NavLink className="nav-link" to="/">
+                                    Add New Habit
+                                </NavLink>
+                            </NavItem>
+                            <NavItem className="nav-item">
+                                <NavLink className="nav-link" to="/view-weekly">
+                                    View Weekly
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                    
+        </Navbar>
     );
 };
 
-export default Navbar;
+export default Header;
